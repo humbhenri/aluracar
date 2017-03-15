2d080deb3139bff0586ac702ec970bcc5ea8a1dd
HUMBERTO HENRIQUE CAMPOS PINHEIRO <humbhenri@gmail.com>
2017-03-15 16:35:27 -0300

    mais rotas e telas

--- AluraCar/www/js/routes.js
+++ AluraCar/www/js/routes.js
@@ -1,10 +1,21 @@
 angular.module('starter')
     .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider.state('listagem', {
-            url: '/listagem',
-            templateUrl: 'template/listagem.html',
-            controller: 'ListagemController'
-        });
+        $stateProvider
+            .state('listagem', {
+                url: '/listagem',
+                templateUrl: 'template/listagem.html',
+                controller: 'ListagemController'
+            })
+            .state('carroescolhido', {
+                url: '/carroescolhido/:carro',
+                templateUrl: 'template/carroescolhido.html',
+                controller: 'CarroEscolhidoController'
+            })
+            .state('finalizarpedido', {
+                url: '/finalizarpedido',
+                templateUrl: 'template/finalizarpedido.html',
+                controller: 'FinalizarPedidoController'
+            });
 
         $urlRouterProvider.otherwise('listagem');
     })
